b6867560845e6bef9986fd08afdcbfb532ef7002
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-07 17:47:20 -0800

    added foundation code for rendering robot array on link click

--- robots/robots.js
+++ robots/robots.js
@@ -1,2 +1,17 @@
 // console.log('henlo');
 import { robots } from '../robots.js';
+import { renderFocusRobot } from '../utils.js';
+
+const robotFocus = document.getElementById('robot-focus');
+
+const params = new URLSearchParams(window.location.search);
+// console.log('robots', robots);
+// console.log('robotFocus', robotFocus);
+// console.log('params', params.get('id'));
+
+const robot = robots.find((item) => item.id === params.get('id'));
+// console.log('robot', robot);
+
+const renderFocus = renderFocusRobot(robot);
+
+robotFocus.append(renderFocus);
