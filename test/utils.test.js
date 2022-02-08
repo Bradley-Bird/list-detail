845b50928bba362240ad054d7edbb852fc03f96f
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-08 08:38:54 -0800

    updated TDD

---
+++ test/utils.test.js
@@ -0,0 +1,15 @@
+import { robots } from '../robots.js';
+import { renderRobots } from '../utils.js';
+const test = QUnit.test;
+
+test('should return list with nested array lists', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div><h2>BENDER</h2><ul><li>Appears In: Futurama</li><li>Quote: "O cruel fate, to be thusly boned! Ask not for whom the bone bones—it bones for thee."</li><li>Fun Fact: Bender would like to kill all humans</li></ul></div>`;
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = renderRobots(robots[0]);
+
+    expect.equal(actual.outerHTML, expected);
+});
