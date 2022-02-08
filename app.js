6b42a2af5bc961014a3437b07abaf5b2fdb8ce76
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-08 15:33:43 -0800

    set up grid areas

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // import functions and grab DOM elements
 import { robots } from './robots.js';
-import { renderRobots } from './utils.js';
+import { renderRobots, renderImg } from './utils.js';
 const robotList = document.getElementById('robotList');
 // let state
 
@@ -10,11 +10,14 @@ const robotList = document.getElementById('robotList');
 // update DOM to reflect the new state
 for (let robot of robots) {
     const idLink = `/robots/?id=${robot.id}`;
-    const idNum = robot.id;
+    // const idNum = robot.id;
     const div = renderRobots(robot);
-    div.classList.add(`robot-${idNum}`);
+    const imgDiv = renderImg(robot);
+    // imgDiv.classList.add(`robot-img-${idNum}`);
+    // div.classList.add(`robot-${idNum}`);
     const a = document.createElement('a');
+    // a.classList.add(`robot-img-${idNum}`);
     a.href = idLink;
-    a.append(div);
-    robotList.append(a);
+    a.append(imgDiv);
+    robotList.append(div,a);
 }
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 export function renderRobots(robot) {
+    const idNum = robot.id;
     const div = document.createElement('div');
+    div.classList.add(`robot-${idNum}`);
     const h2 = document.createElement('h2');
     h2.textContent = robot.name.toUpperCase();
     const ul = document.createElement('ul');
@@ -10,9 +12,18 @@ export function renderRobots(robot) {
         li.textContent = item;
         ul.append(li);
     }
-
     div.append(h2, ul);
     return div;
+}
+export function renderImg(robot) {
+    const idNum = robot.id;
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add(`robot-img-${idNum}`);
+    const img = document.createElement('img');
+    img.src = `./assets/${robot.name}.png`;
+
+    imgDiv.append(img);
+    return imgDiv;
 }
 export function renderFocusRobot(robot) {
     const div = document.createElement('div');
