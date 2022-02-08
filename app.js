ed9129c0295876e8a8cc4e74e9cd1595b93075a1
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-08 14:47:20 -0800

    set up dynamic classes for css

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const robotList = document.getElementById('robotList');
 // use user input to update state
 // update DOM to reflect the new state
 for (let robot of robots) {
+    const idLink = `/robots/?id=${robot.id}`;
+    const idNum = robot.id;
     const div = renderRobots(robot);
+    div.classList.add(`robot-${idNum}`);
     const a = document.createElement('a');
-    a.href = `/robots/?id=${robot.id}`;
+    a.href = idLink;
     a.append(div);
     robotList.append(a);
 }
