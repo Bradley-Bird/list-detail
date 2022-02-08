d1649903a2ae5dbb56aca52e2212d7f4e4a516bf
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-07 17:03:17 -0800

    eslint

--- robots.js
+++ robots.js
@@ -2,25 +2,25 @@ export const robots = [
     {
         name: 'Bender',
         info: [
-            "Appears In: 'Futurama'",
-            "Quote: 'O cruel fate, to be thusly boned! Ask not for whom the bone bones—it bones for thee.'",
-            "Fun Fact: Bender would like to kill all humans",
+            'Appears In: Futurama',
+            'Quote: "O cruel fate, to be thusly boned! Ask not for whom the bone bones—it bones for thee."',
+            'Fun Fact: Bender would like to kill all humans',
         ],
     },
     {
         name: 'RoboCop',
         info: [
-            "Appears In: 'RoboCop'",
-            "Quote: 'My friends call me Murphy. You call me...RoboCop'",
-            "Fun Fact: 'RoboCop wants to protect most humans'",
+            'Appears In: RoboCop',
+            'Quote: "My friends call me Murphy. You call me...RoboCop"',
+            'Fun Fact: RoboCop wants to protect most humans',
         ],
     },
     {
         name: 'Iron Giant',
         info: [
-            "Appears In: 'The Iron Giant'",
-            "Quote: 'You stay. I go. No following.'",
-            "Fun Fact: 'Vin Diesel is the voice of The Iron Giant'",
+            'Appears In: The Iron Giant',
+            'Quote: "You stay. I go. No following."',
+            'Fun Fact: Vin Diesel is the voice of The Iron Giant',
         ],
     },
 ];
